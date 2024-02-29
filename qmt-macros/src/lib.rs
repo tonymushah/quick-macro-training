b3424a2d22f1cdc22d446e167c2eddeb651f7296
tonymushah <tonyramanantsalama@gmail.com>
2024-02-29 10:31:16 +0300

    Just saving

--- qmt-macros/src/lib.rs
+++ qmt-macros/src/lib.rs
@@ -1,6 +1,15 @@
 use proc_macro::{self, TokenStream};
+use syn::{parse as syn_parse, Item, parse_macro_input, Attribute};
 
-#[proc_macro]
-pub fn my_macro(token: TokenStream) -> TokenStream {
-    token
+#[proc_macro_attribute]
+pub fn tony(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let item = parse_macro_input!(item as Item);
+    match item {
+        Item::Fn(fun) => {
+            fun.sig.fn_token.
+        }
+        _ => {
+            panic!("Excepted function")
+        }
+    }
 }
